0c11a1a1c795d0656ee81243c8e889eb44de6437
Kepchuk <outube1995@gmail.com>
2026-04-09 16:26:35 +0300

    feat: implements user controller endpoints

---
+++ src/main/java/com/example/shop/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.shop.controller;
+
+import com.example.shop.dto.user.ChangePasswordRequest;
+import com.example.shop.dto.user.ChangeRoleRequest;
+import com.example.shop.dto.user.CreateUserRequest;
+import com.example.shop.dto.user.UserResponse;
+import com.example.shop.entity.Role;
+import com.example.shop.entity.User;
+import com.example.shop.mapper.UserMapper;
+import com.example.shop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public UserResponse getUserById(@PathVariable Long userId) {
+        return UserMapper.toUserResponse(userService.getUserById(userId));
+    }
+
+    @GetMapping("/email")
+    public UserResponse getUserByEmail(@RequestParam String email) {
+        return UserMapper.toUserResponse(userService.getUserByEmail(email));
+    }
+
+    @GetMapping
+    public List<UserResponse> getUsersByRole(@RequestParam Role role) {
+        return UserMapper.toUserResponseList(userService.getUsersByRole(role));
+    }
+
+    @PostMapping
+    public UserResponse createUser(@RequestBody CreateUserRequest request) {
+        User user = userService.createUser(request.getEmail(), request.getPassword(), request.getRole());
+        return UserMapper.toUserResponse(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUserById(@PathVariable Long userId) {
+        userService.deleteUserById(userId);
+    }
+
+    @PatchMapping("/{userId}/password")
+    public UserResponse changePassword(@PathVariable Long userId, @RequestBody ChangePasswordRequest request) {
+        return UserMapper.toUserResponse(userService.changePassword(userId, request.getNewPassword()));
+    }
+
+    @PatchMapping("/{userId}/role")
+    public UserResponse changeRole(@PathVariable Long userId, @RequestBody ChangeRoleRequest request) {
+        return UserMapper.toUserResponse(userService.changeRole(userId, request.getNewRole()));
+    }
+}
