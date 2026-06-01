c268fb15a97ce5774d0dbcb17fea7f5c7e8c0ef3
Kepchuk <outube1995@gmail.com>
2026-06-01 14:09:19 +0300

    refactor(domain): change product collections from List to Set to avoid bag issues

--- src/main/java/com/example/shop/entity/Product.java
+++ src/main/java/com/example/shop/entity/Product.java
@@ -3,7 +3,6 @@ package com.example.shop.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.BatchSize;
 
 import java.util.*;
 
@@ -27,13 +26,11 @@ public class Product {
     @Column(nullable = false)
     private ProductCategory category;
 
-    @BatchSize(size = 20)
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ProductImage> productImages = new ArrayList<>();
+    private Set<ProductImage> productImages = new LinkedHashSet<>();
 
-    @BatchSize(size = 20)
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ProductVariant> variants = new ArrayList<>();
+    private Set<ProductVariant> variants = new LinkedHashSet<>();
 
     public Product(String name, String description, ProductCategory category) {
         validateName(name);
